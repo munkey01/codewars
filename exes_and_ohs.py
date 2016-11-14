c2e5fd2b42590e69e3b5868677fac797adcf601b
munkey01 <joeydgrant@gmail.com>
2016-11-14 11:53:11 -0500

    Create exes_and_ohs.py

---
+++ exes_and_ohs.py
@@ -0,0 +1,12 @@
+'''
+Challenge: 
+Return True if number of x's and o's are equal in a string. Otherwise, return False. Should not be case sensitive.
+
+Things learned:
+By putting 'and' instead of '==' I would get True if x's and o's were both present, no matter the number of them.
+This must be due to a return of any number by count being treated as a True value, therefore True and True returns True for xo().
+By putting '==' in instead of 'and' I force the function to determine if the two values are equal to one another.
+'''
+
+def xo(s):
+    return list(s.lower()).count('x') == list(s.lower()).count('o')
